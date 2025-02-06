a6b0ce360348555f172303d7849645f17f524925
Ivan Núñez Rodriguez <ivannr20@gmail.com>
2025-02-06 14:43:36 +0100

    modfica la accion de esc

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 public enum GameState { Ready, Countdown, Boss, Victory, Defeat }
 public class GameManager : MonoBehaviour
 {
+     public static bool isPaused = false;
+    public GameObject pauseMenuUI;
     public static GameManager Instance { get; private set; }
     public GameState CurrentGameState { get; private set; }
     private float _timeRemaining = 180.0f;
@@ -52,12 +54,47 @@ public class GameManager : MonoBehaviour
             CurrentGameState = GameState.Boss;
         }
     }
-      void Update()
+        void Update()
     {
-        // Si se presiona la tecla ESC, volver al menú principal
+        // Si se presiona la tecla ESC, pausar o reanudar el juego
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene("MainMenu"); // Asegúrate de que "MainMenu" es el nombre de tu escena principal
+            if (!isPaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
         }
     }
+
+    public void ResumeGame()
+    {
+        if (pauseMenuUI != null)
+        {
+            pauseMenuUI.SetActive(false);
+        }
+
+        Time.timeScale = 1f;
+        isPaused = false;
+        ResumeCountdown();
+    }
+
+    public void PauseGame()
+    {
+        if (pauseMenuUI != null)
+        {
+            pauseMenuUI.SetActive(true);
+        }
+
+        Time.timeScale = 0f;
+        isPaused = true;
+        PauseCountdown();
+    }
+
+  
+
+    
 }
